81844ee29b81755bb19f987d21c2c626ad1d02fe
Alexander Schepanovski <suor.web@gmail.com>
2014-08-26 16:18:42 +0800

    Add non-efficiency note

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ Conf.prototype = {
     query: function (query, params, callback) {
         // TODO: deal with absense of params or even callback
         this.run(function (client, callback) {
-            client.query(query, params, callback);
+            client.query(query, params, callback); // Don't need to instrument this
         }, callback);
     },
 
