23a4ee032efa29ea1911d7c101781aa36962ed09
Alexander Schepanovski <suor.web@gmail.com>
2014-08-27 17:19:43 +0800

    Separate point free async flow utils

--- index.js
+++ index.js
@@ -1,57 +1,7 @@
 var debug = require('debug')('pg-bricks');
+var pf = require('point-free');
 var sql = require('sql-bricks');
 var pg = require('pg');
-
-
-var pf = {
-    waterfall: function () {
-        // TODO: check tasks types?
-        var tasks = [].slice.call(arguments);
-        var index = -1;
-        var callback;
-
-        function handler(err) {
-            if (err) return callback(err);
-            index++;
-            if (index >= tasks.length) return callback.apply(null, arguments);
-
-            var args = [].slice.call(arguments, 1);
-            tasks[index].apply(null, args.concat([handler]))
-        }
-
-        return function () {
-            var args = [].slice.call(arguments);
-            callback = args.pop();
-
-            handler.apply(null, [null].concat(args));
-        };
-    },
-
-    series: function () {
-        var tasks = [].slice.call(arguments);
-        var results = [];
-        var index = -1;
-        var args, callback;
-
-        function handler(err) {
-            if (err) return callback(err);
-            index++;
-            // TODO: handle no results / more than 1 result
-            if (index) results.push(arguments[1]);
-            if (index >= tasks.length) return callback(null, results);
-
-            tasks[index].apply(null, args.concat([handler]))
-        }
-
-        return function () {
-            args = [].slice.call(arguments);
-            callback = args.pop();
-
-            handler.apply(null, [null].concat(args));
-        };
-
-    }
-}
 
 
 function _expectRow(res, callback) {
